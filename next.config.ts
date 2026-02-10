a20677a8b12ac3e5a9f5ed922515ad464bf374f3
emma <emmahan0303@gmail.com>
2026-02-10 17:15:26 +0800

    git init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //防止你寫出「死連結」，大幅減少 404 錯誤
+  typedRoutes: true,
 };
 
 export default nextConfig;
