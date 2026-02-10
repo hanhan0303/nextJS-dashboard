4add978e97be33c8215e15d320cf716151bd3c1c
emma <emmahan0303@gmail.com>
2026-02-10 17:42:31 +0800

    fix: <Link> href 型別調整

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -77,7 +77,7 @@ function PaginationNumber({
   return isActive || position === 'middle' ? (
     <div className={className}>{page}</div>
   ) : (
-    <Link href={href} className={className}>
+    <Link href={href as any} className={className}>
       {page}
     </Link>
   );
@@ -112,7 +112,7 @@ function PaginationArrow({
   return isDisabled ? (
     <div className={className}>{icon}</div>
   ) : (
-    <Link className={className} href={href}>
+    <Link className={className} href={href as any}>
       {icon}
     </Link>
   );
