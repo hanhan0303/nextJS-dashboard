1fbf7996fbaeb63830096ba6621727fe2070e6bb
emma <emmahan0303@gmail.com>
2026-02-23 15:42:01 +0800

    學習完成

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next';
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
@@ -36,3 +37,7 @@ export default async function Page(props: {
     </div>
   );
 }
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
